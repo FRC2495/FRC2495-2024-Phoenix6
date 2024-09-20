55b1725e435a1b74105d69345c061a3b76fdb576
frc2495 <frc2495@gmail.com>
2024-09-20 15:04:19 -0400

    temporary magic division number change

--- src/main/java/frc/robot/util/Magic.java
+++ src/main/java/frc/robot/util/Magic.java
@@ -4,6 +4,6 @@ public class Magic {
 	
 	public static double getEncoderCounts(double distance)
 	{
-		return -((-6.8622*distance*distance+1542.1*distance-17642)-5000)/2048; //divide by 2048 to convert from ticks to revolutions
+		return -((-6.8622*distance*distance+1542.1*distance-17642)-5000)/1650; // /2048; //divide by 2048 to convert from ticks to revolutions
 	}
 }
