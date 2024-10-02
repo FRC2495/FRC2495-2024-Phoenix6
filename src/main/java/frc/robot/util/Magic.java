093740342672252cdb5e3130a72aa4e20f0c3c0d
frc2495 <frc2495@gmail.com>
2024-10-02 16:23:12 -0400

    changed magic formula

--- src/main/java/frc/robot/util/Magic.java
+++ src/main/java/frc/robot/util/Magic.java
@@ -4,6 +4,7 @@ public class Magic {
 	
 	public static double getEncoderCounts(double distance)
 	{
-		return -((-6.8622*distance*distance+1542.1*distance-17642)-5000)/2048; //divide by 2048 to convert from ticks to revolutions
+		return -(-0.0011*distance*distance+0.5374*distance-3.9787); //-((-6.8622*distance*distance+1542.1*distance-17642)-5000)/2048; //divide by 2048 to convert from ticks to revolutions
 	}
 }
+
