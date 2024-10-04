5ab088e07a7dfac95001f50c7f7da5d32f702842
FRC2495 <frc2495@gmail.com>
2024-10-04 15:57:43 -0400

    Changed the equation in the magic shoot

--- src/main/java/frc/robot/util/Magic.java
+++ src/main/java/frc/robot/util/Magic.java
@@ -4,7 +4,7 @@ public class Magic {
 	
 	public static double getEncoderCounts(double distance)
 	{
-		return -(-0.0011*distance*distance+0.5374*distance-3.9787); //-((-6.8622*distance*distance+1542.1*distance-17642)-5000)/2048; //divide by 2048 to convert from ticks to revolutions
+		return -(-0.0041*distance*distance+0.8679*distance-12.204); //-((-6.8622*distance*distance+1542.1*distance-17642)-5000)/2048; //divide by 2048 to convert from ticks to revolutions
 	}
 }
 
